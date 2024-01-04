5d43e15397ad9c5c3bb6cd6348cd86497a1de9dc
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-01-04 22:44:34 +0200

    fixed typos

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ import sys
 av = sys.argv
 l_av = len(av) - 1
 
-if l_av >= 1:
+if l_av > 1:
     print(l_av, 'arguments:')
     for i in range(1, l_av + 1):
         print('{:d}: {}'.format(i, av[i]))
+elif l_av == 1:
+    print(l_av, 'argument:')
+    print('{:d}: {}'.format(1, av[1]))
 elif l_av == 0:
     print(l_av, 'arguments.')
