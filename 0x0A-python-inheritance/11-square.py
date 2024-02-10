d3d5f2e4ae51473c212e8036ef3bf25a32d19a82
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 21:34:27 +0200

    11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     Square Geometry calculation
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         self.integer_validator(size, size)
         super().__init__(size, size)
         self.__size = size
