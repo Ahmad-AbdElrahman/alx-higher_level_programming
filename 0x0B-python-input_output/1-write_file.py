70f1abeb2bed7275a90d33e2c3185b872b548bc7
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 22:34:40 +0200

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+writing to file
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    writing to file
+    """
+
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
