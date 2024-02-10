c5cfc1967493514645d3518b394b8f8084b30db0
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 20:52:07 +0200

    8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
+"""
+Geometry calculation
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    Checks a integer value
+
+    Args:
+        name (str): The name of the value.
+        value (int): The value.
+
+    Raises:
+        TypeError: If `value` isn't a integer.
+        ValueError: If `value` is less than or equal to zero.
+    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
