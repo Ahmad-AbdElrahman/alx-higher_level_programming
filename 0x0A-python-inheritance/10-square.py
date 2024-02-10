dcf232822bab8e204c34bd4ba160f6ee2bc9d7a6
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 21:17:33 +0200

    10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Square Geometry calculation
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square Geometry calculation
+    """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
