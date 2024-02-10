eb249cd970c673185a15d780f5e2b04aab950100
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 22:38:30 +0200

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+appending to file
+"""
+
+
 def append_write(filename="", text=""):
+    """
+    appending to file
+    """
+
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
