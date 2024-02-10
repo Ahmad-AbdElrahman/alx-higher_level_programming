b7e65e40a8d884aca8f670b2f3d5290558b62339
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 19:43:18 +0200

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+returns True if the object is an instance
+"""
+
+
 def inherits_from(obj, a_class):
+    """
+    returns True if the object is an instance
+    """
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
