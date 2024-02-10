107564bb0a7d14c6829f1bff1413daa8d79a177a
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-10 22:27:16 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+printing to stdout
+"""
+
+
 def read_file(filename=""):
+    """
+    printing to stdout
+    """
+
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
