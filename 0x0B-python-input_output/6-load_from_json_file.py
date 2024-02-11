96fc4a97eddb7e746b0f061bdbd4990621f4a027
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-11 18:15:55 +0200

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+loads in json rep
+"""
+
+
 from json import loads
 
 
 def load_from_json_file(filename):
+    """
+    loads in json rep
+    """
+
     with open(filename, encoding='utf-8') as f:
         return loads(f.read())
