7518401db0401d57c31aa8c73533ec7e1499616c
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-11 18:03:21 +0200

    4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+from json to python
+"""
+
+
 from json import loads
 
 
 def from_json_string(my_str):
+    """
+    representation in json
+    """
+
     return loads(my_str)
