54891c3b348904b768fca6bd2a1768f80ec24e95
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-11 18:01:21 +0200

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+representation in json
+"""
+
+
 from json import dumps
 
 
 def to_json_string(my_obj):
+    """
+    string to json representation
+    """
+
     return dumps(my_obj)
