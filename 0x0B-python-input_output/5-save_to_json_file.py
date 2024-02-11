a92bf266d276669bf4ed38aa8a5d95022cc3b50f
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-02-11 18:11:17 +0200

    5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+saving in json rep
+"""
+
+
 from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    saving in json rep
+    """
+
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(dumps(my_obj))
