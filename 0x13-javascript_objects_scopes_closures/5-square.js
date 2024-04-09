7db654e1265f978027eca1c647ec481a955f6553
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2024-04-09 16:22:42 -0700

    tasks

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require("./4-rectangle");
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-<<<<<<< HEAD
   constructor (size) {
     super(size, size);
     this.size = size;
@@ -12,15 +11,6 @@ class Square extends Rectangle {
   double () {
     super.double();
   }
-=======
-    constructor(size) {
-        super(size, size);
-    }
-
-    double () {
-        super.double();
-    }
->>>>>>> ae25f2f7537e02a27544e49bcb41e789c8e00cf5
 }
 
 module.exports = Square;
