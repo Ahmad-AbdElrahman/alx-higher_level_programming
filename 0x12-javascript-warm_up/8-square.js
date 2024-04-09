d37a2605affbda1a0e5da214b4d56a9372bf6be5
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-04-09 05:57:32 +0200

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+if (Number(process.argv[2])) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    let string = ''
+    for (let i = 0; i < process.argv[2]; i++) {
+      string += 'x'
+    }
+  console.log(string);
+  }
+} else {
+  console.log('Missing size');
+}
