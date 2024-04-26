85d27fa1991f73cf672e48368607970c9340f733
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-04-27 00:43:38 +0300

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
-
-link="$1"
-response=$(curl -sI "$url")
-content_length=$(echo "$response" | awk '/content_length/ {print $2}' | tr -d '\r')
+# This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
+curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
